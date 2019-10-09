edaec88244ed98dea863f52b607ac680c6b92d72
peterihimire <peterihimire@gmail.com>
2019-10-09 13:46:07 +0100

    More changes added horizontal line to the middle of the heading and more

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
-let mainNav = document.querySelector('#js-menu');
-let navBarToggle = document.querySelector('#js-navbar-toggle')
+let mainNav = document.querySelector('.main-nav');
+let navBarToggle = document.querySelector('.navbar-toggle')
 
 navBarToggle.addEventListener('click', () => {
     mainNav.classList.toggle('active');
+});
+
+navBarToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('change');
 });
