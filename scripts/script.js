15a27d65d6aa44c5d6098f9f6bce60be1c48dde2
peterihimire <peterihimire@gmail.com>
2019-10-11 15:25:02 +0100

    added the glider.js files to create the carousel , then I modified the about page to ,and added the css to make it look better via p tag and margin bottom

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,15 @@ let navBarToggle = document.querySelector('#js-navbar-toggle')
 navBarToggle.addEventListener('click', () => {
     mainNav.classList.toggle('active');
 });
+
+
+
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 4,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
